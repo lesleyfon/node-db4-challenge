28fa274f9f40c980e28f9c6a2aef5c2d28efbe8a
lesley <lesleybanadzem2017@gmail.com>
2020-01-10 14:55:34 -0800

    Created instrction that accepts step instruction note recipe_instruction_id from recipe_table

--- data/migrations/20200110132739_initial.js
+++ data/migrations/20200110132739_initial.js
@@ -8,15 +8,22 @@ exports.up = async function(knex) {
             tbl.string('ingredient_name');
     })
     await knex.schema.createTable('recipe_ingredients', tbl => {
+        // tbl.increments('id')
         tbl.integer('recipe_id').unsigned().notNullable().references('id').inTable('recipes');
         tbl.integer('ingredient_id').unsigned().notNullable().references('id').inTable('ingredients');
         tbl.string('quantity');
-        tbl.primary(['recipe_id', 'ingredient_id']);
-})
-    
+        tbl.increments('id').primary(['recipe_id', 'ingredient_id']);
+    })
+    await knex.schema.createTable('instructions', tbl => {
+        tbl.integer('step');
+        tbl.string('instruction_note');
+        tbl.integer('recipe_instruction_id').unsigned().notNullable().references('recipe_id').inTable('recipe_ingredients');
+        tbl.increments('id');
+    })
 };
 
 exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists('instructions');
     await knex.schema.dropTableIfExists('recipe_ingredients');
     await knex.schema.dropTableIfExists('recipes');
     await knex.schema.dropTableIfExists('ingredients');
--- data/seeds/00_clean_Up.js
+++ data/seeds/00_clean_Up.js
@@ -1,4 +1,5 @@
 exports.seed = async function(knex){
+    await knex('instructions').truncate();
     await knex('recipe_ingredients').truncate();
     await knex('recipes').truncate();
     await knex('ingredients').truncate();
