a2d5599de29934eeee1f4dd1176918c2ca678a92
lesley <lesleybanadzem2017@gmail.com>
2020-01-10 13:01:37 -0800

    created schema for recipe and ingredients and drop both tables in down function

--- data/migrations/20200110125040_initial.js
+++ data/migrations/20200110125040_initial.js
@@ -1,8 +1,17 @@
 
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+    await knex.schema.createTable('recipes', tbl => {
+        tbl.increments();
+        tbl.string('recipe_name', 128)
+     })
+    await knex.schema.createTable('ingredients', tbl => {
+            tbl.increments();
+            tbl.string('ingredient_name');
+    })
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+    await knex.schema.dropTableIfExist('recipes');
+    await knex.schema.dropTableIfExist('ingredients_name');
+
 };
